8ed5f94b3f2d573c6887c5c8bb3771a6cfd05d03
Thais-Gonzaga <devthaisgonza@gmail.com>
2022-11-09 19:23:49 -0300

    feat: criando botão configuraçoes
     ref: #requisito 3

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from 'react';
 import './App.css';
 import { Route, Switch } from 'react-router-dom';
 import Login from './pages/Login';
+import Settings from './pages/Settings';
 
 export default function App() {
   return (
     <Switch>
       <Route path="/" component={ Login } exact />
+      <Route exact path="/settings" component={ Settings } />
     </Switch>
   );
 }
