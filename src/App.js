23c8fda725db5808824b28e16c1926bb43a18874
alexandrebelem21 <alexandrebelem21@gmail.com>
2022-11-09 15:27:01 -0300

    feat release

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ export default function App() {
       <header className="App-header">
         <img src={ logo } className="App-logo" alt="logo" />
         <p>SUA VEZ</p>
+        <p>.</p>
       </header>
     </div>
   );
