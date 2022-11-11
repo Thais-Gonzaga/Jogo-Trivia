453a213e898bd86e448de7ac433bce29b6224498
Nathalia <nathalia.cantarelli@edu.pucrs..br>
2022-11-10 21:18:27 -0300

    Fix: Requisito 9 corrigindo a store do cypress

--- src/redux/store.js
+++ src/redux/store.js
@@ -4,7 +4,7 @@ import thunk from 'redux-thunk';
 import rootReducer from './reducers';
 
 const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(thunk)));
-// if (window.Cypress) {
-//   window.store = store;
-// }
+if (window.Cypress) {
+  window.store = store;
+}
 export default store;
